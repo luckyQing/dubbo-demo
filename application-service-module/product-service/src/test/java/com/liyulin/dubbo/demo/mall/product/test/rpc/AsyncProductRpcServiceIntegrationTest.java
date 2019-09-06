571c71a8eb3d859a7a42e99ce37cf19a8e864eff
liyulin <1634753825@qq.com>
2019-09-06 20:43:07 +0800

    完善集成测试

--- application-service-module/product-service/src/test/java/com/liyulin/dubbo/demo/mall/product/test/rpc/AsyncProductRpcServiceIntegrationTest.java
+++ application-service-module/product-service/src/test/java/com/liyulin/dubbo/demo/mall/product/test/rpc/AsyncProductRpcServiceIntegrationTest.java
@@ -7,25 +7,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import com.liyulin.demo.sdk.test.AbstractIntegrationTest;
-import com.liyulin.dubbo.demo.mall.rpc.product.ProductRpc;
+import com.liyulin.dubbo.demo.mall.rpc.product.AsyncProductRpc;
 import com.liyulin.dubbo.demo.mall.rpc.product.request.ProductSearchReqBody;
 import com.liyulin.dubbo.demo.mall.rpc.product.response.QryProductByIdRespBody;
 
-public class ProductRpcServiceIntegrationTest extends AbstractIntegrationTest {
+public class AsyncProductRpcServiceIntegrationTest extends AbstractIntegrationTest {
 
 	@Reference(validation = "true")
-	private ProductRpc productRpc;
+	private AsyncProductRpc asyncProductRpc;
 	
 	@Test
 	public void testQryById() {
-		QryProductByIdRespBody product = productRpc.qryById(1L);
+		QryProductByIdRespBody product = asyncProductRpc.qryById(1L);
 		Assertions.assertThat(product).isNotNull();
 	}
 	
 	@Test
 	public void testSearch() throws Exception {
 		ProductSearchReqBody productSearchReqBody = ProductSearchReqBody.builder().name("phone").price(3L).build();
-		List<QryProductByIdRespBody> products = productRpc.search(productSearchReqBody);
+		List<QryProductByIdRespBody> products = asyncProductRpc.search(productSearchReqBody);
 		
 		Assertions.assertThat(products).isNotEmpty();
 	}
