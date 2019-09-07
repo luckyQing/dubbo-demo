1967856785b77bf09d97bec3ae6bd9d6837ec67a
luckyQing <1634753825@qq.com>
2019-09-07 12:17:51 +0800

    修改异步测试

--- application-service-module/order-service/src/test/java/com/liyulin/dubbo/demo/mall/order/test/product/AsyncProductControllerIntegrationTest.java
+++ application-service-module/order-service/src/test/java/com/liyulin/dubbo/demo/mall/order/test/product/AsyncProductControllerIntegrationTest.java
@@ -2,12 +2,15 @@ package com.liyulin.dubbo.demo.mall.order.test.product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.fastjson.TypeReference;
@@ -27,6 +30,12 @@ public class AsyncProductControllerIntegrationTest extends AbstractIntegrationTe
 	@InjectMocks
 	private AsyncProductService asyncProductService;
 	
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+	
+	// FIXME:
 	@Test
 	public void testAsyncByRpcContext() throws Exception {
 		// stubbing qryById
@@ -40,6 +49,26 @@ public class AsyncProductControllerIntegrationTest extends AbstractIntegrationTe
 		ProductSearchReqBody searchReqBody = ProductSearchReqBody.builder().name("phone").price(3L).build();
 		AsyncResultReqBody asyncResultReqBody = AsyncResultReqBody.builder().id(1L).search(searchReqBody).build();
 		AsyncResultRespBody asyncResultRespBody = super.post("/order/asyncProduct/asyncByRpcContext", asyncResultReqBody,
+				new TypeReference<AsyncResultRespBody>() {
+				});
+		Assertions.assertThat(asyncResultRespBody).isNotNull();
+		Assertions.assertThat(asyncResultRespBody.getProduct()).isNotNull();
+		Assertions.assertThat(asyncResultRespBody.getProducts()).isNotEmpty();
+	}
+	
+	@Test
+	public void testAsyncByCompletableFuture() throws Exception {
+		// stubbing asyncQryById
+		Mockito.when(asyncProductRpc.asyncQryById(Mockito.anyLong())).thenReturn(CompletableFuture.completedFuture(new QryProductByIdRespBody()));
+
+		// stubbing asyncSearch
+		List<QryProductByIdRespBody> mockProducts = new ArrayList<>();
+		mockProducts.add(QryProductByIdRespBody.builder().name("test").price(3L).build());
+		Mockito.when(asyncProductRpc.asyncSearch(Mockito.any())).thenReturn(CompletableFuture.completedFuture(mockProducts));
+		
+		ProductSearchReqBody searchReqBody = ProductSearchReqBody.builder().name("phone").price(3L).build();
+		AsyncResultReqBody asyncResultReqBody = AsyncResultReqBody.builder().id(1L).search(searchReqBody).build();
+		AsyncResultRespBody asyncResultRespBody = super.post("/order/asyncProduct/asyncByCompletableFuture", asyncResultReqBody,
 				new TypeReference<AsyncResultRespBody>() {
 				});
 		Assertions.assertThat(asyncResultRespBody).isNotNull();
